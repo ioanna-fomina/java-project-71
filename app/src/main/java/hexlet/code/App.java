163053561f9674abbab095dddaa300b1269ddc59
Ioanna Fomina <ioanna.fomina.software@gmail.com>
2023-02-18 16:37:23 +0300

    add diff for flat jsons

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,4 +1,5 @@
 package hexlet.code;
+//import java.io.;
 
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
@@ -6,19 +7,29 @@ import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
 import java.io.File;
+import java.util.concurrent.Callable;
 
 @Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
         description = "Compares two configuration files and shows a difference.")
-public class App {
+public class App implements Callable<String> {
 
-    @Parameters(index = "file1", description = "path to first file")
+    @Parameters(index = "0", description = "path to first file")
     private File filepath1;
 
-    @Parameters(index = "file2", description = "path to second file")
+    @Parameters(index = "1", description = "path to second file")
     private File filepath2;
 
     @Option(names = {"-f", "--format"}, description = "output format [default: stylish]")
     private String format;
+
+    @Override
+    public String call() throws Exception {
+        File file1 = filepath1;
+        File file2 = filepath2;
+        String diff = Differ.generate(file1, file2);
+        System.out.println(diff);
+        return diff;
+    }
 
     public static void main(String... args) {
         int exitCode = new CommandLine(new App()).execute(args);
---
+++ app/src/main/java/hexlet/code/Differ.java
@@ -0,0 +1,58 @@
+package hexlet.code;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.stream.Collectors;
+
+//import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class Differ {
+    static Map<String, Object> parse(Map<String, Object> data1, Map<String, Object> data2) {
+        Set<String> keys = new TreeSet<>(Comparator.naturalOrder());
+        keys.addAll(data1.keySet());
+        keys.addAll(data2.keySet());
+        Map<String, Object> result = new LinkedHashMap<>();
+
+        for (String key: keys) {
+            if (!data1.containsKey(key)) {
+                result.put("+ " + key, data2.get(key));
+            } else if (!data2.containsKey(key)) {
+                result.put("- " + key, data1.get(key));
+            } else if (data1.get(key).equals(data2.get(key))) {
+                result.put("  " + key, data1.get(key));
+            } else {
+                result.put("- " + key, data1.get(key));
+                result.put("+ " + key, data2.get(key));
+            }
+        }
+        return result;
+    }
+    static Map<String, Object> getMap(File file) throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        Map<String, Object> map = mapper.readValue(file, Map.class);
+        return map;
+    }
+    static String entryToString(Map.Entry<String, Object> item) {
+        return item.getKey() + ": " + item.getValue().toString();
+    }
+    public static String generate(File file1, File file2) throws Exception {
+        Path path1 = Paths.get(file1.toURI()).toAbsolutePath().normalize();
+        Path path2 = Paths.get(file2.toURI()).toAbsolutePath().normalize();
+
+        if (!Files.exists(path1)) {
+            throw new Exception("File '" + file1 + "' does not exist");
+        }
+        if (!Files.exists(path2)) {
+            throw new Exception("File '" + file2 + "' does not exist");
+        }
+        Map<String, Object> result = parse(getMap(file1), getMap(file2));
+        return result.entrySet().stream()
+                .map(Differ::entryToString)
+                .collect(Collectors.joining("\n"));
+    }
+}
