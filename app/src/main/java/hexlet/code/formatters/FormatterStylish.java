c7da3f562a0836580f84234f098662168233faa8
Ioanna Fomina <ioanna.fomina.software@gmail.com>
2023-02-25 16:06:47 +0300

    make corrections

--- app/src/main/java/hexlet/code/formatters/FormatterStylish.java
+++ app/src/main/java/hexlet/code/formatters/FormatterStylish.java
@@ -38,30 +38,4 @@ public class FormatterStylish {
         }
         return "{" + result + "\n}";
     }
-    /*public static String stylish(Map<String, String> keys, Map<String, Object> data1, Map<String, Object> data2) {
-        var result = new StringBuilder();
-
-        for (Map.Entry<String, String> entry: keys.entrySet()) {
-            String key = entry.getKey();
-
-            switch (entry.getValue()) {
-                case "added":
-                    result.append("\n  + " + key + ": " + data2.get(key));
-                    break;
-                case "removed":
-                    result.append("\n  - " +  key + ": " + data1.get(key));
-                    break;
-                case "unchanged":
-                    result.append("\n    " + key + ": " + data1.get(key));
-                    break;
-                case "updated":
-                    result.append("\n  - " + key + ": " + data1.get(key));
-                    result.append("\n  + " + key + ": " + data2.get(key));
-                    break;
-                default:
-                    break;
-            }
-        }
-        return "{" + result + "\n}";
-    }*/
 }
